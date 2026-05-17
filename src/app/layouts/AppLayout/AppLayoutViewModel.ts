772cf67750e6abbd7f96f689e82d8f388466bf31
anton62k <anton.kashirov@gmail.com>
2026-05-17 10:04:53 +0700

    fix: resolve sonar duplicate import

--- src/app/layouts/AppLayout/AppLayoutViewModel.ts
+++ src/app/layouts/AppLayout/AppLayoutViewModel.ts
@@ -2,11 +2,11 @@ import { makeAutoObservable } from 'mobx';
 
 import {
   type ActiveNavigationItem,
+  type IViewModel,
   type NavigationItem,
   primaryNavItems,
   withActivePrimaryNavigationItems,
 } from 'src/shared/config';
-import type { IViewModel } from 'src/shared/config';
 import { container } from 'src/shared/lib';
 import { LocaleService, type LocaleOption, type SupportedLocale } from 'src/shared/model';
 
